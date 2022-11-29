930f5d07abb805db524dfbab2d1cabd1148f0259
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-11-29 02:48:37 -0300

    feat: initial next structure with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      '2xl': 24,
+      '3xl': 32
+    },
+    colors: {
+      black: '#000',
+      chestnut: {
+        600: '#B7524E'
+      },
+      cyan: {
+        600: '#69AEDE',
+        700: '#5098C9'
+      },
+      gray: {
+        100: '#C4C4CC',
+        200: '#E1E1E6',
+        400: '#7C7C8A',
+        800: '#202024',
+        900: '#121214'
+      },
+      transparent: 'transparent',
+      white: '#fff'
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
+  },
+  plugins: []
+};
